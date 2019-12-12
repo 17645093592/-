9b4c9a1dd5218fcae855025e33e27844bd7aeb3d
weijiale <a15834112139@163.com>
2019-12-12 22:30:39 +0800

    123

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react'
 import Header from "./components/header"
-import {Route,Redirect} from "react-router-dom"
-import {maxRoutes,maiRoutes} from "./router"
+import { Route, Redirect } from "react-router-dom"
+import { maxRoutes, maiRoutes } from "./router"
+
 export default class App extends Component {
     render() {
         return (
             <>
                 <div className="rootTwo">
-                <Header></Header>
+                    <Header></Header>
                     {
-                            maxRoutes.map(router=>{
-                                return <Route key={router.pathname} path={router.pathname} component={router.component} />
-                            })
+                        maxRoutes.map(router => {
+                            return <Route key={router.pathname} path={router.pathname} component={router.component} />
+                        })
                     }
-                  
-                    <Redirect to={maxRoutes[0].pathname} from = "/" exact></Redirect>
-               
+
+                    <Redirect to={maxRoutes[2].pathname} from="/" exact></Redirect>
+
                 </div>
                 {
-                    maiRoutes.map(router=>{
+                    maiRoutes.map(router => {
                         return <Route key={router.pathname} path={router.pathname} component={router.component} />
                     })
                 }
-                    </>     
+            </>
         )
     }
 }
---
+++ src/pages/shopping/components/top.js
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from 'react'
+import { Icon, Button } from "antd"
+
+export default class top extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div className="shoop-top-box">
+                    <Button>
+                        <Icon type="search" />
+                        搜索电影正版周边
+                    </Button>
+                    <Icon type="shopping-cart" className="shoop-top-right" />
+                </div>
+            </Fragment>
+        )
+    }
+}
